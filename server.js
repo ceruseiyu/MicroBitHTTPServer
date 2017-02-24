1594dafa41b17e002257c545e322d3a848786f6d
Ceru <eurocracy2@gmail.com>
2017-02-24 21:05:37 +0000

    Reading of characteristics for testing working

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 var noble = require('noble');
 
-var serviceUUID = "1351634a09d1484699b9ee3112c3f55b";
+var serviceUUID = '1351634a09d1484699b9ee3112c3f55b';
+var urlCharacteristicUUID = '13513f0209d1484699b9ee3112c3f55b';
+var requestCharacteristicUUID = '1351149e09d1484699b9ee3112c3f55b';
+
+var url = 'EMPTY';
 
 function startScan(state) {
 	if(state === 'poweredOn') {
@@ -11,22 +15,34 @@ function startScan(state) {
 }
 
 function checkMicroBit(services) {
-	for (var i in services) {
-		if(services[i].uuid === serviceUUID) {
-			return true;
-		}
+	if(services[0].uuid === serviceUUID) {
+		return true;
 	}
 	return false;
 }
+	
 
 function connectService(peripheral) {
-	peripheral.connect(function errorCallback(error) {
-		peripheral.discoverServices(null, function(error, services) {
+	noble.stopScanning();
+	peripheral.connect(function deviceConnect(error) {
+		peripheral.discoverServices(serviceUUID, function accessServivces(error, services) {
 			if(checkMicroBit(services)) {
-				console.log('Connected to Micro:bit');
+				console.log('Connected to Micro:bit with HTTP Service');
+				HTTPService = services[0];
+				
+				HTTPService.discoverCharacteristics(null, function accessCharacteristics(error, characteristics) {
+					for (var i in characteristics) {
+						console.log(characteristics[i].uuid);
+						
+					}
+					characteristics[i].read(function(error, data) {
+						console.log('+' + data.toString('utf8') + '+');
+					});
+				});
 			} else {
 				console.log('Connected to unknown device');
 				peripheral.disconnect(function(error) {});
+				noble.startScanning();
 			}
 		});
 	});
